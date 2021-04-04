833e05e2e03fd59b59d9d1039c958468ce964061
Leonardo Fernandes <leonardo.l.fernandes@gmail.com>
2021-04-04 20:09:42 -0300

    inicializando o projeto

---
+++ src/main/java/com/lambdasys/heroes/api/HeroesApiApplication.java
@@ -0,0 +1,13 @@
+package com.lambdasys.heroes.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeroesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeroesApiApplication.class, args);
+	}
+
+}
