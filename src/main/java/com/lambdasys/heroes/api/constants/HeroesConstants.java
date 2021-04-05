a8d10d1ee231e80d204ffbfcf3133a1ff2eb6970
Leonardo Fernandes <leonardo.l.fernandes@gmail.com>
2021-04-05 00:03:35 -0300

    Constantes para configuração do dynamo client db.

---
+++ src/main/java/com/lambdasys/heroes/api/constants/HeroesConstants.java
@@ -0,0 +1,10 @@
+package com.lambdasys.heroes.api.constants;
+
+public class HeroesConstants {
+
+    public static final String HEROES_TABLE_NAME = "HEROES_TABLE" ;
+    public static final String HEROES_ENDPOINT = "/heroes" ;
+    public static final String DYNAMO_ENDPOINT = "http://localhost:8000";
+    public static final String DYNAMO_REGION = "us-east-1" ;
+
+}
