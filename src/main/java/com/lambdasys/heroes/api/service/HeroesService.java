11b63e42782eb029c5ae9a98bf6a7e4d4b44d200
Leonardo Fernandes <leonardo.l.fernandes@gmail.com>
2021-04-05 19:02:27 -0300

    Implementação de métodos de buscar todos, salvar, excluir, localizar por id

--- src/main/java/com/lambdasys/heroes/api/service/HeroesService.java
+++ src/main/java/com/lambdasys/heroes/api/service/HeroesService.java
@@ -1,8 +1,11 @@
 package com.lambdasys.heroes.api.service;
 
+import com.lambdasys.heroes.api.document.Heroes;
 import com.lambdasys.heroes.api.repository.HeroesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Service
 public class HeroesService {
@@ -13,4 +16,26 @@ public class HeroesService {
        this.repository = repository;
    }
 
+   public Flux<Heroes> findAll(){
+       return Flux.fromIterable(this.repository.findAll());
+   }
+
+   public Mono<Heroes> findById(final String id){
+       return Mono.justOrEmpty(this.repository.findById(id));
+   }
+
+   public Mono<Heroes> save(final Heroes heroes){
+       return Mono.justOrEmpty(this.repository.save(heroes));
+   }
+
+   public Mono<Boolean> deleteById(final String id){
+        this.repository.deleteById(id);
+        return Mono.just(true);
+   }
+
+   public Mono<Boolean> delete(final Heroes heroes){
+       this.repository.delete(heroes);
+       return Mono.just(true);
+   }
+   
 }
