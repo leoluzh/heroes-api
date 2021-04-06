65cd1bd777b48f0ec23fdbb7aa019b01a9ed58b6
Leonardo Fernandes <leonardo.l.fernandes@gmail.com>
2021-04-05 22:36:08 -0300

    Refactoring para controle de mangas.

--- src/main/java/com/lambdasys/mangas/api/config/MangasTable.java
+++ src/main/java/com/lambdasys/mangas/api/config/MangasTable.java
@@ -1,4 +1,4 @@
-package com.lambdasys.heroes.api.config;
+package com.lambdasys.mangas.api.config;
 
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
@@ -6,7 +6,7 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.dynamodbv2.document.Table;
 import com.amazonaws.services.dynamodbv2.model.*;
-import com.lambdasys.heroes.api.constants.HeroesConstants;
+import com.lambdasys.mangas.api.constants.MangasConstants;
 import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,22 +14,22 @@ import java.util.List;
 
 @Configuration
 @EnableDynamoDBRepositories
-public class HeroesTable {
+public class MangasTable {
 
     public static void main( String... args) throws Exception {
 
         AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
                 .withEndpointConfiguration(
                         new AwsClientBuilder.EndpointConfiguration(
-                                HeroesConstants.DYNAMO_ENDPOINT,
-                                HeroesConstants.DYNAMO_REGION))
+                                MangasConstants.DYNAMO_ENDPOINT,
+                                MangasConstants.DYNAMO_REGION))
                 .build();
 
         DynamoDB dynamoDB = new DynamoDB(client);
 
         try{
             Table table = dynamoDB.createTable(
-                    HeroesConstants.HEROES_TABLE_NAME,
+                    MangasConstants.MANGAS_TABLE_NAME,
                     List.of( new KeySchemaElement("id", KeyType.HASH)),
                     List.of( new AttributeDefinition("id", ScalarAttributeType.S)),
                     new ProvisionedThroughput(5L,5L));
