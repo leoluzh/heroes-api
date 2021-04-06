dd8cf8edce08b85eea6568ffe170742f6433bfe1
Leonardo Fernandes <leonardo.l.fernandes@gmail.com>
2021-04-05 22:37:58 -0300

    Refactoring para atender entidade manga.

---
+++ src/main/java/com/lambdasys/mangas/api/service/MangasService.java
@@ -0,0 +1,41 @@
+package com.lambdasys.mangas.api.service;
+
+import com.lambdasys.mangas.api.document.Mangas;
+import com.lambdasys.mangas.api.repository.MangasRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+public class MangasService {
+
+   private final MangasRepository repository;
+
+   public MangasService(@Autowired final MangasRepository repository){
+       this.repository = repository;
+   }
+
+   public Flux<Mangas> findAll(){
+       return Flux.fromIterable(this.repository.findAll());
+   }
+
+   public Mono<Mangas> findById(final String id){
+       return Mono.justOrEmpty(this.repository.findById(id));
+   }
+
+   public Mono<Mangas> save(final Mangas mangas){
+       return Mono.justOrEmpty(this.repository.save(mangas));
+   }
+
+   public Mono<Boolean> deleteById(final String id){
+        this.repository.deleteById(id);
+        return Mono.just(true);
+   }
+
+   public Mono<Boolean> delete(final Mangas mangas){
+       this.repository.delete(mangas);
+       return Mono.just(true);
+   }
+
+}
