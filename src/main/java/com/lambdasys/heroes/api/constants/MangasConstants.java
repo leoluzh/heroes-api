8f267b9734fe09e9697c803d19ebe5e9a0414074
Leonardo Fernandes <leonardo.l.fernandes@gmail.com>
2021-04-05 21:26:27 -0300

    Inicio de refactoring - transformando projeto em gerenciamento de mangas.

--- src/main/java/com/lambdasys/heroes/api/constants/MangasConstants.java
+++ src/main/java/com/lambdasys/heroes/api/constants/MangasConstants.java
@@ -1,9 +1,9 @@
 package com.lambdasys.heroes.api.constants;
 
-public class HeroesConstants {
+public class MangasConstants {
 
-    public static final String HEROES_TABLE_NAME = "HEROES_TABLE" ;
-    public static final String HEROES_ENDPOINT = "/heroes" ;
+    public static final String MANGAS_TABLE_NAME = "MANGAS_TABLE" ;
+    public static final String MANGAS_ENDPOINT = "/api/v1/heroes" ;
     public static final String DYNAMO_ENDPOINT = "http://localhost:8000";
     public static final String DYNAMO_REGION = "us-east-1" ;
 
