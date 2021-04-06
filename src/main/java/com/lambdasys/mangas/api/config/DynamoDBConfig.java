277023bb5e7e605520394029eb4911a5873e00d5
Leonardo Fernandes <leonardo.l.fernandes@gmail.com>
2021-04-06 13:54:20 -0300

    Refactoring da classe para suporte a region. Atualização por meio da pagina oficial.

--- src/main/java/com/lambdasys/mangas/api/config/DynamoDBConfig.java
+++ src/main/java/com/lambdasys/mangas/api/config/DynamoDBConfig.java
@@ -1,45 +1,71 @@
 package com.lambdasys.mangas.api.config;
 
 import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
 import org.apache.commons.lang3.StringUtils;
+import org.socialsignin.spring.data.dynamodb.repository.config.DynamoDBMapperConfigFactory;
 import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
-@ConfigurationProperties(prefix ="amazon.dynamo")
-@EnableDynamoDBRepositories
+@EnableDynamoDBRepositories(basePackages = "com.lambdasys.mangas.api.repository")
 public class DynamoDBConfig {
 
-    @Value("${endpoint}")
-    private String endpoint;
+    @Value("${amazon.dynamodb.endpoint}")
+    private String amazonAWSEndpoint;
+
+    @Value("${amazon.dynamodb.region}")
+    private String amazonAWSRegion;
 
     @Value("${amazon_aws_accesskey}")
-    private String accessKey;
+    private String amazonAWSAccessKey;
 
     @Value("${amazon_aws_secretkey}")
-    private String secretKey;
+    private String amazonAWSSecretKey;
 
-    @Bean
-    public AmazonDynamoDB amazonDynamoDB(){
-        var amazonDynamoDB = new AmazonDynamoDBClient(awsCredentials());
-        if( StringUtils.isNotEmpty( endpoint ) ){
-            amazonDynamoDB.setEndpoint(endpoint);
-        }
-        return amazonDynamoDB;
+    public AWSCredentialsProvider amazonAWSCredentialsProvider() {
+        return new AWSStaticCredentialsProvider(amazonAWSCredentials());
     }
 
+    @Primary
     @Bean
-    public AWSCredentials awsCredentials(){
-        return new BasicAWSCredentials(
-                this.accessKey ,
-                this.secretKey
-        );
+    public AWSCredentials amazonAWSCredentials() {
+        return new BasicAWSCredentials(amazonAWSAccessKey, amazonAWSSecretKey);
+    }
+
+    @Primary
+    @Bean
+    public DynamoDBMapperConfig dynamoDBMapperConfig() {
+        return DynamoDBMapperConfig.DEFAULT;
+    }
+
+    @Primary
+    @Bean
+    public DynamoDBMapper dynamoDBMapper(AmazonDynamoDB amazonDynamoDB, DynamoDBMapperConfig config) {
+        return new DynamoDBMapper(amazonDynamoDB, config);
+    }
+
+    @Primary
+    @Bean
+    public AmazonDynamoDB amazonDynamoDB() {
+        return AmazonDynamoDBClientBuilder.standard().withCredentials(amazonAWSCredentialsProvider())
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(
+                        amazonAWSEndpoint ,
+                        amazonAWSRegion
+                )).build();
     }
 
 }
